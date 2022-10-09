7c596c2c4ee4b1255de82ce4c82d51be79fdd5e3
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2022-10-09 11:11:48 +0530

    object.js
    Added objects in javaScript

---
+++ object.js
@@ -0,0 +1,13 @@
+let cap = {
+  name: "Steve",
+  surname: "Rogers",
+  age: 39,
+  adress: {
+    city: "Kolkata",
+    street: "Park Street",
+    moholla: "Mere aspas ka",
+  },
+  movies: ["First Avenger", "Winter soldier", "civil War"],
+  isAvenger: true,
+};
+console.log("cap: ", cap);
